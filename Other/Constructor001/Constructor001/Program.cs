2c2a77db18b30f389a3780b1ffa79dec903c67fa
DruiD <omsk.petr@gmail.com>
2019-11-04 13:20:08 +0600

    Add Other Folder

---
+++ Other/Constructor001/Constructor001/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            Console.ReadLine();
+        }
+    }
+
+    public class A
+    {
+        static A()
+        {
+            Console.WriteLine("Static constractor of A class ");
+        }
+
+        public A()
+        {
+            Console.WriteLine("Normal constractor of A class ");
+        }
+    }
+
+
+    public class B : A 
+    {
+        static B()
+        {
+            Console.WriteLine("Static constractor of B class ");
+        }
+
+        public B()
+        {
+            Console.WriteLine("Normal constractor of B class ");
+        }
+    }
+}
---
+++ Other/Constructor001/TryCathEx/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TryCathEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ex = new TryCatchExeption();
+            try
+            {
+                ex.Do();
+            }
+            catch (Exception exeption)
+            {
+                Console.WriteLine(exeption);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class TryCatchExeption
+    {
+        public void Do()
+        {
+            try
+            {
+                var first = 1; //1
+                var second = 0;//0
+                var third = first / second;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("throw Exeption in Try");
+                throw new NullReferenceException();
+            }
+            finally
+            {
+                Console.WriteLine("throw Exeption in Finally");
+                throw new ArgumentException();
+            }
+        }
+
+    }
+}
