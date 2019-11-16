c16949b388613eb9ad65fa5d697f948ad84d6da4
DruiD <omsk.petr@gmail.com>
2019-11-16 08:45:48 +0600

    Refactoring

--- Other/Constructor001/LINQ/Program.cs
+++ Other/Constructor001/LINQ/Program.cs
@@ -11,12 +11,10 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-
             DateTime startTime = DateTime.Now;
   
             List<string> list = new List<string>();
             list = File.ReadAllLines("2.txt").ToList();
-
 
             var filter = list.Where(x => x != "a");
             filter = filter.Where(x => x != "b");
@@ -27,13 +25,11 @@ namespace LINQ
             foreach (string itel in filter)
                 count++;
 
-
             var comeNew = filter.First() + filter.Last();
 
             DateTime stopTime = DateTime.Now;
             TimeSpan timeWorked = new TimeSpan();
             timeWorked = stopTime - startTime;
-
 
             Console.WriteLine(string.Format("{0:0}:{1:00}", (int)timeWorked.Seconds, timeWorked.Milliseconds));
             Console.ReadLine();
