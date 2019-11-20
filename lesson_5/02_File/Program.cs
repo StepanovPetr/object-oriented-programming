4f1734c971be73cdf098a4f1470f38065c12d0f9
DruiD <omsk.petr@gmail.com>
2019-11-20 15:59:54 +0600

    UPDATE lesson_5

---
+++ lesson_5/02_File/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // необходимое пространство имен для работы с файлама, папками, дисками и файловыми потокамиnamespace _02_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример создания файла.
+            File.Create("Example.txt").Dispose();
+
+            // Пример добавления текста в конец файла.
+            File.AppendAllText("Example.txt", "Some text");
+            
+            // Пример копирования файла.
+            File.Copy("Example.txt","Example2.txt");
+            
+            // Пример переименования, Перемещения файла.
+            File.Move("Example2.txt", "Example3.txt");
+            
+            // Пример проверки существования файла.
+            if (File.Exists("Example.txt"))
+                //Пример удаления файла.
+                File.Delete("Example.txt");
+        }
+    }
+}
---
+++ lesson_5/03_File/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // необходимое пространство имен для работы с файлама, папками, дисками и файловыми потоками
+namespace _03_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пример создания файла.
+            File.Create("Example.txt").Dispose();
+
+            // Пример добавления текста в конец файла.
+            File.AppendAllText("Example.txt", "Some text");
+
+            // Получение аттрибутов файла.
+            FileAttributes fileAttributes = File.GetAttributes("Example.txt");
+
+        }
+    }
+}
