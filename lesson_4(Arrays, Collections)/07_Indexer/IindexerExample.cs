725a59c694944bfabfef4b161eeb6e7eb75613e0
DruiD <omsk.petr@gamil.com>
2019-11-26 22:04:30 +0600

    UPDATE lesson_4

--- lesson_4(Arrays, Collections)/07_Indexer/IindexerExample.cs
+++ lesson_4(Arrays, Collections)/07_Indexer/IindexerExample.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace lesson_4
+﻿namespace lesson_4
 {
     interface IindexerExample
     {
