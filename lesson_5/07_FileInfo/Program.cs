95ebd2f65ba402a413029d2ccf3dba145b6b2db2
DruiD <omsk.petr@gmail.com>
2019-12-10 16:37:19 +0600

    Update Lesson_5

---
+++ lesson_5/07_FileInfo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+https://docs.microsoft.com/en-us/dotnet/api/system.io.fileinfo.copyto?view=netframework-4.8
+namespace _07_FileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fileInfo = new FileInfo("Example.txt");
+            fileInfo.CopyTo("Example.txt.bak");
+            fileInfo.CopyTo("ExampleNew.txt.bak");
+            //fileInfo.CopyTo("ExampleNew.txt.bak");
+            fileInfo.CopyTo("ExampleNew.txt.bak",true);
+
+            fileInfo.MoveTo("Example.MoveTo.txt");
+
+        }
+    }
+}
