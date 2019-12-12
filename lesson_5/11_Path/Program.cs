f094603bdd42bb155165f7eff5922e142f9b05c7
DruiD <omsk.petr@gmail.com>
2019-12-12 17:28:04 +0600

    UPDATE Lesson_5

---
+++ lesson_5/11_Path/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace _11_Path
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i < 10; i++)
+                Console.WriteLine(Path.GetRandomFileName());
+
+            Console.WriteLine("Получение дирректории для хранения временных файлов");
+            Console.WriteLine(Path.GetTempPath());
+
+            Console.WriteLine("Создание временного файла и возвращение его имени");
+            Console.WriteLine(Path.GetTempFileName());
+
+            Console.ReadLine();
+        }
+    }
+}
