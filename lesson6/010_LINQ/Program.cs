5e0d18c2a8241bdff4739881aad1e950f56432c2
DruiD <omsk.petr@gamil.com>
2020-02-20 20:53:32 +0600

    Change lesson6.

---
+++ lesson6/010_LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_LINQ
+{
+    class Program
+    {
+        // Выборка значений.
+        static void Main(string[] args)
+        {
+            List<Man> persons = new List<Man>() { Man.GetMan(), Man.GetMan(), Man.GetMan(), Man.GetMan(), Man.GetMan(), Man.GetMan(), Man.GetMan(), Man.GetMan(), Man.GetMan(), Man.GetMan() };
+
+            var queryMenByFirstName =
+            from person in persons
+            select new { person.FirstName, person.Age};
+
+            foreach (var person in queryMenByFirstName)
+            {
+                Console.WriteLine("{0}-{1}",person.FirstName, person.Age );
+            }
+            Console.ReadLine();
+        }
+    }
+}
