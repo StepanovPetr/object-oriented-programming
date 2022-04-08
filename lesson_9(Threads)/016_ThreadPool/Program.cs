7748554b820e68adaee94e977f5cb8679547c8e6
DruiD <omsk.petr@gamil.com>
2022-04-08 14:38:02 +0600

    Update lesson9.

---
+++ lesson_9(Threads)/016_ThreadPool/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace _016_ThreadPool
+{
+    internal class Program
+    {
+        // Метод который будет выполняться в отдельном потоке.
+        private static void MethodForThread(object id)
+        {
+            Console.WriteLine(Thread.CurrentThread.IsThreadPoolThread);
+            Thread.CurrentThread.Priority = ThreadPriority.Highest;
+
+            Console.WriteLine($"{id}  started");
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{id} normal thread work");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine($"{id} finished");
+        }
+
+        static void Main(string[] args)
+        {
+            int countThread;
+            int completionPortThreads;
+
+            ThreadPool.GetAvailableThreads(out countThread, out completionPortThreads);
+            Console.WriteLine($"The number of available worker threads - {countThread}, number of available asynchronous I/O threads {completionPortThreads}");
+
+            ThreadPool.GetMaxThreads(out countThread, out completionPortThreads);
+            Console.WriteLine($"The number of MAX worker threads - {countThread}, number of MAX asynchronous I/O threads {completionPortThreads}");
+
+            ThreadPool.GetMinThreads(out countThread, out completionPortThreads);
+            Console.WriteLine($"The number of MIN worker threads - {countThread}, number of MIN asynchronous I/O threads {completionPortThreads}");
+
+            ThreadPool.UnsafeQueueUserWorkItem(MethodForThread, "001");
+            ThreadPool.UnsafeQueueUserWorkItem(MethodForThread, "002");
+            ThreadPool.UnsafeQueueUserWorkItem(MethodForThread, "003");
+
+            ThreadPool.GetAvailableThreads(out countThread, out completionPortThreads);
+            Console.WriteLine($"The number of available worker threads - {countThread}, number of available asynchronous I/O threads {completionPortThreads}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lesson_9(Threads)/017_ConcurrentBag/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+// Пространство имен для работы с конкурентными коллекциями.
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace _017_ConcurrentBag
+{
+    internal class Program
+    {
+        // Пример потокобезопасной коллекции.
+        static ConcurrentBag<string> concurrentBag = new ConcurrentBag<string>();
+
+        // Метод который будет выполняться в отдельном потоке.
+        private static void MethodForThread(object id)
+        {
+            Console.WriteLine($"{id}  started");
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{id} normal thread work");
+                Thread.Sleep(1000);
+                concurrentBag.Add($"{id} - {i}");
+            }
+
+            Console.WriteLine($"{id} finished");
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.UnsafeQueueUserWorkItem(MethodForThread, "001");
+            ThreadPool.UnsafeQueueUserWorkItem(MethodForThread, "002");
+            ThreadPool.UnsafeQueueUserWorkItem(MethodForThread, "003");
+            ThreadPool.UnsafeQueueUserWorkItem(MethodForThread, "004");
+
+            Thread.Sleep(11000);
+            foreach (var item in concurrentBag)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
