6382dcc5ecd161da971fceca23b62391909720ef
DruiD <omsk.petr@gmail.com>
2024-10-04 14:52:53 +0600

    Added mock example.

---
+++ src/lesson_3(Abstract Classes, Interface)/UnitTests/DeaneryTests.cs
@@ -0,0 +1,24 @@
+﻿using LibExample.DependencyInjection;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DeaneryTests
+    {
+        [Test]
+
+        public void Deanery_ObjectOrientedProgramming_ShouldEqualOrMore90()
+        {
+            // Arrange.
+            DepartmentMock departmentMock = new DepartmentMock();
+            Deanery deanery = new Deanery(departmentMock);
+
+            // Act.
+            var student = deanery.SetStudentnetAssessment("");
+
+            //Assert.
+            Assert.That(student.Scholarship, Is.EqualTo(100000));
+        }
+    }
+}
---
+++ src/lesson_3(Abstract Classes, Interface)/UnitTests/DepartmentMock.cs
@@ -0,0 +1,14 @@
+﻿using LibExample.DependencyInjection;
+
+namespace UnitTests;
+
+public class DepartmentMock : IDepartment
+{
+    public Student GetStudent(string email)
+    {
+        return new Student()
+        {
+            ObjectOrientedProgramming = 90
+        };
+    }
+}
