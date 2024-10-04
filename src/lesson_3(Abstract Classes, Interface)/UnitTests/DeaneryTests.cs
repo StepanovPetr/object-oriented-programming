cf21fbe02fe6b746757fb2f3ed13e5a389e3a2c9
DruiD <omsk.petr@gmail.com>
2024-10-04 18:09:51 +0600

    Added moq exmple.

--- src/lesson_3(Abstract Classes, Interface)/UnitTests/DeaneryTests.cs
+++ src/lesson_3(Abstract Classes, Interface)/UnitTests/DeaneryTests.cs
@@ -1,4 +1,5 @@
 ﻿using LibExample.DependencyInjection;
+using Moq;
 using NUnit.Framework;
 
 namespace UnitTests
@@ -6,13 +7,32 @@ namespace UnitTests
     [TestFixture]
     public class DeaneryTests
     {
-        [Test]
-
+        [Test(Description = "")]
         public void Deanery_ObjectOrientedProgramming_ShouldEqualOrMore90()
         {
             // Arrange.
             DepartmentMock departmentMock = new DepartmentMock();
             Deanery deanery = new Deanery(departmentMock);
+
+            // Act.
+            var student = deanery.SetStudentnetAssessment("");
+
+            //Assert.
+            Assert.That(student.Scholarship, Is.EqualTo(100000));
+        }
+
+        [Test(Description ="")]
+        public void Deanery_OperatingSystems_ShouldEqualOrMore90()
+        {
+            // Arrange.
+            Mock<IDepartment> departmentMock = new();
+            departmentMock.Setup(m => m.GetStudent(It.IsAny<string>()))
+                .Returns(new Student()
+            {
+                OperatingSystems = 90
+            });
+
+            Deanery deanery = new Deanery(departmentMock.Object);
 
             // Act.
             var student = deanery.SetStudentnetAssessment("");
